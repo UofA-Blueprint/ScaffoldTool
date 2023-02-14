223549f8874b981d10f13cfb64d5d409ac8b556f
Jared Drueco <drueco03@gmail.com>
2023-02-13 19:52:08 -0700

    Initialized node app with typescript and commander. Log initial message.

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const figlet = require("figlet");
+console.log(figlet.textSync("Welcome to the Blueprint Scaffolding Tool"));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const figlet = require('figlet');
+
+console.log(figlet.textSync('Welcome to the Blueprint Scaffolding Tool'));
